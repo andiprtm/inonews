4ed6d98070a74039efa0b9ce7da90d439033492c
andiprtm <andipratama15011@gmail.com>
2025-09-19 23:54:29 +0700

    fix(api): change setup

--- app/src/main/java/com/cpp/inonews/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/cpp/inonews/data/remote/retrofit/ApiService.kt
@@ -8,37 +8,5 @@ import retrofit2.converter.gson.GsonConverterFactory
 import com.cpp.inonews.BuildConfig
 
 interface ApiService {
-    companion object{
-        private const val BASE_URL = BuildConfig.BASE_URL
 
-        fun getApiService(): ApiService {
-
-            val loggingInterceptor = if (BuildConfig.DEBUG) {
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            } else {
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-            }
-
-            val authInterceptor = Interceptor { chain ->
-                val req = chain.request()
-                val requestHeaders = req.newBuilder()
-                    .addHeader("Authorization", BuildConfig.NEWS_API)
-                    .build()
-                chain.proceed(requestHeaders)
-            }
-
-            val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .addInterceptor(authInterceptor)
-                .build()
-
-            val retrofit = Retrofit.Builder()
-                .baseUrl( BASE_URL )
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-
-            return retrofit.create(ApiService::class.java)
-        }
-    }
 }
